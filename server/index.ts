76456468e61d245c1655f6263ce6341ad1769135
Ryan Scharfer <rscharfer@gmail.com>
2026-04-20 18:19:24 -0700

    remove redundancy

--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,6 @@
 import { serve } from "@hono/node-server";
-import { serveStatic } from "@hono/node-server/serve-static";
 
 import app from "./app";
-
-app.use("/*", serveStatic({ root: "./public" }));
 
 serve({ fetch: app.fetch, port: 3000 }, () => {
 	console.log("spun up server");
